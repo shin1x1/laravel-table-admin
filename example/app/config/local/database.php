2a6409e439da0c7f502c970abbe31d3eed9e0d12
shin1x1 <shin1x1@gmail.com>
2014-05-17 18:01:22 +0900

    add new files

---
+++ example/app/config/local/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'app',
+            'username' => 'root',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ],
+    ],
+
+];
---
+++ example/app/config/testing/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'app_test',
+            'username' => 'root',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ],
+    ],
+
+];
